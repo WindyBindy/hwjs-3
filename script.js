da60311303c52a9fc73fdef11d316a9bf263836b
WindyBindy <nikita2021novsk@gmail.com>
2025-04-10 07:31:27 +0200

    d

---
+++ script.js
@@ -0,0 +1,16 @@
+const result = 5 + 5 + '5'
+console.log(result)
+console.log(typeof result)
+
+const email = 'fsdfasdf@gmail.com'
+console.log(email.includes('@'))
+console.log(email.length)
+
+const first = 'My'
+const second = 'name'
+const third = 'is'
+let fullName = `${first} ${second} ${third} Viktor`
+
+const userName = 'Олександро'
+const payment = 300
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`)
